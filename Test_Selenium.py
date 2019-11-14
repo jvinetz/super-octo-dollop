5ee470abc898f7adb38ec3286cbb2742052069d0
« maikel987 » <maikelsitbon@gmail.com>
2019-11-14 15:10:46 +0200

    adding drive

--- Test_Selenium.py
+++ Test_Selenium.py
@@ -4,35 +4,35 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-webdriver = r"drive/chromedriver"
-driver = Chrome(webdriver)
 
-url = "https://www.waytostay.com/paris-apartments/"
-driver.get(url)
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+def main():
+    webdriver = r"drive/chromedriver"
+    driver = Chrome(webdriver)
 
-info = driver.find_elements_by_class_name("tile")
-prices = []
-details = []
-for j in range(len(info)):
+    url = "https://www.waytostay.com/paris-apartments/"
+    driver.get(url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    info = driver.find_elements_by_class_name("tile")
+    prices = []
+    details = []
+    for j in range(len(info)):
         prices.append(driver.find_elements_by_class_name('price-person')[j].text)
         details.append(driver.find_elements_by_class_name('concise-details')[j].text)
-print(prices, details)
-driver.close()
-driver.quit()
+    print(prices, details)
+    driver.close()
+    driver.quit()
 
 
+def test():
+    assert (lambda x: x + 1)(1) == 2
 
 
+if __name__ == '__main__':
+    test()
+    main()
 
-
-
-
-
-
-
-
-#SACADO DE UN TUTORIAL
+# SACADO DE UN TUTORIAL
 # total = []
 # for item in range(items):
 #     quotes = driver.find_elements_by_class_name("quote")
--- WebScraping.py
+++ WebScraping.py
@@ -4,30 +4,43 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-source = requests.get('https://www.waytostay.com/')
-print(source.status_code)  # 200 means the page was downloaded successfully
-#source = source.text
 
-soup = BeautifulSoup(source, 'lxml')
-print(soup.prettify()) #prints the 'inspect' of the hole page
+def main():
+    source = requests.get('https://www.waytostay.com/')
+    print(source.status_code)  # 200 means the page was downloaded successfully
+    #source = source.text
 
-# csv_file = open('TripAdvisor.csv', 'w')
-# csv_writer = csv.writer(csv_file)
-# csv_writer.writerow(['Price', 'Contact info'])
+    soup = BeautifulSoup(source, 'lxml')
+    print(soup.prettify()) #prints the 'inspect' of the hole page
 
-# find place page
-page = soup.find('div', class_="destination-info").a
-raw_data = str(page)
-page = re.search('href=(.*)/', raw_data).group()
-web_page = "https://www.waytostay.com" + page[6:]
-print(web_page)
+    # csv_file = open('TripAdvisor.csv', 'w')
+    # csv_writer = csv.writer(csv_file)
+    # csv_writer.writerow(['Price', 'Contact info'])
 
-new_source = requests.get(web_page)
-new_source = new_source.text
-new_soup = BeautifulSoup(source, 'lxml')
-element = new_soup.find('div', class_="large-6 columns")
-print(element)
+    # find place page
+    page = soup.find('div', class_="destination-info").a
+    raw_data = str(page)
+    page = re.search('href=(.*)/', raw_data).group()
+    web_page = "https://www.waytostay.com" + page[6:]
+    print(web_page)
+
+    new_source = requests.get(web_page)
+    new_source = new_source.text
+    new_soup = BeautifulSoup(source, 'lxml')
+    element = new_soup.find('div', class_="large-6 columns")
+    print(element)
 
 
 #     csv_writer.writerow([prices[i], phones[i]])
 # csv_file.close()
+
+
+
+
+def test():
+    assert (lambda x: x + 1)(1) == 2
+
+
+if __name__ == '__main__':
+    test()
+    main()
