c7f56e9090ebea725614487f517221febe84b08c
« maikel987 » <maikelsitbon@gmail.com>
2019-12-03 22:28:23 +0200

    MySQL implementation

--- DB1.py
+++ DB1.py
@@ -1,7 +1,19 @@
 import DB
 import pandas as pd
 
-df = pd.read_csv(r'csv/data.csv')
-df = df.values.tolist()
-#print(df['curency'].unique())
-DB.first_fill(df)
+data = pd.read_csv(r'csv/data1.csv')
+data = data.iloc[:,1:]
+'''print(data)
+
+curr_unique = data['curency'].unique().tolist()
+curr_list = [[i, curr_unique[i]] for i in range(len(curr_unique))]
+curr = {i[1]: i[0] for i in curr_list}
+
+data['curency'] = data['curency'].apply(lambda x: curr[x])
+data['bedrooms'] = data['bedrooms'].apply(lambda x: int(x) if x != 'studio' else 0)
+data_prep_list = data.values.tolist()
+for i in range(len(data_prep_list)):
+    data_prep_list[i].insert(0, i)
+print(data_prep_list)'''
+
+DB.first_fill(data)
